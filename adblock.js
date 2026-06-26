206e18a4e4c8830395ea0b422e673aeb3433141f
GitHub Action <action@github.com>
2026-06-26 03:47:05 +0000

    Auto-fix formatting issues

--- adblock.js
+++ adblock.js
@@ -1,8 +1,10 @@
-setInterval(() => { 
-    if(document.getElementsByClassName("ytp-ad-text").length > 0) { 
-        const video = document.getElementsByClassName("video-stream html5-main-video")[0]; 
-        video.play(); 
-        video.pause(); 
-        video.currentTime = video.duration; 
-    } 
+setInterval(() => {
+  if (document.getElementsByClassName("ytp-ad-text").length > 0) {
+    const video = document.getElementsByClassName(
+      "video-stream html5-main-video",
+    )[0];
+    video.play();
+    video.pause();
+    video.currentTime = video.duration;
+  }
 }, 500);
