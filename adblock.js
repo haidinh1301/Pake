140467f9e94c802e21b643ff53986dc1cb3e2550
haidinh1301 <haidinh.work@gmail.com>
2026-06-26 10:46:39 +0700

    Create adblock.js
    adblock functions for youtube
    
    Signed-off-by: haidinh1301 <haidinh.work@gmail.com>

---
+++ adblock.js
@@ -0,0 +1,8 @@
+setInterval(() => { 
+    if(document.getElementsByClassName("ytp-ad-text").length > 0) { 
+        const video = document.getElementsByClassName("video-stream html5-main-video")[0]; 
+        video.play(); 
+        video.pause(); 
+        video.currentTime = video.duration; 
+    } 
+}, 500);
